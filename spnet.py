6a9ff4cfe3acdb29c89fc6e63a92fe12cae7ddab
Zane Pei <zane@promote.cache-dns.local>
2015-06-08 00:58:38 +0800

    provide an example

--- spnet.py
+++ spnet.py
@@ -1,5 +1,5 @@
 from brian2 import *
-
+prefs.codegen.target = 'numpy'
 
 class BaseNeuronGroup(NeuronGroup):
     def __init__(self, N, a, b, c, d, isInhibitory=False, threshold_voltage=30*mV):
@@ -16,7 +16,8 @@ class BaseNeuronGroup(NeuronGroup):
         ''' % (c, d)
 
         t = super().__init__(N, model=self._model, threshold='v >= %f*mV' % (threshold_voltage/mV), reset=self._reset)
-        self.u = self.v = self.I = 0
+        self.u =  self.I = 0
+        self.v = c*mV
 
 class RSNeuronGroup(BaseNeuronGroup):
     def __init__(self, N):
@@ -30,47 +31,40 @@ class FSNeuronGroup(BaseNeuronGroup):
 NE = 3
 NI = 1
 
-#P = RSNeuronGroup(3)
-mymodel = '''
-        dv/dt = (0.04*(v/mV)**2 + 5*v/mV + 140 - u/mV + I/mV)*mV / ms : volt
-        du/dt = 0.02*(0.2*v-u) / ms: volt
-        dI/dt = -log(2)*I/ms: volt 
-'''
-myreset = '''
-        v = %f * mV
-        u = u + %f * mV
-''' % (-65, 8)
-P = NeuronGroup(3, model = mymodel) 
+P = RSNeuronGroup(3)
 Q = RSNeuronGroup(1)
-G = SpikeGeneratorGroup(2, array([0,0]), array([3,6])*ms)
+G = SpikeGeneratorGroup(1, array([0,0]), array([500, 800])*ms)
 
-SG1 = Synapses(G, P, model = 'w : volt', pre='I_post += w')
+SG1 = Synapses(G, P, model = 'w : volt', pre='I += w')
 SG2 = Synapses(G, Q, model = 'w : volt', pre='I += w')
-SG1.w = 100*mV
-SG2.w = 100*mV
-SI = Synapses(Q, P, model = 'w : volt', pre='I -= w')
-SE = Synapses(P, P, model = 'w : volt', pre='I += w')
+
+SI = Synapses(Q, P, model = '''w : volt''', pre='''I -= w''')
+SE = Synapses(P, P, model = '''w : volt''', pre='''I += w''')
 
 SG1.connect([0], [0])
 SG2.connect([0], [0])
+SG1.w = 30*mV
+SG2.w = 30*mV
 SE.connect([0, 0], [1, 2])
 SI.connect([0, 0], [1, 2])
+SE.w = array([30, 30])*mV
+SI.w = array([20, 20])*mV
+SE.delay = array([10, 10])*ms
+SI.delay = array([10, 30])*ms
 
 MS = SpikeMonitor(G)
 ME = StateMonitor(P, ('v','u','I'), record=[0, 1, 2])
 MI = StateMonitor(Q, ('v',), record=[0])
 
-run(10*ms)
+run(1000*ms)
 
 subplot(2, 2, 1)
-plot(ME.t/ms, ME.v[0], label='v')
+plot(ME.t/ms, ME.v[0]/mV, label='v0')
 subplot(2, 2, 2)
-plot(ME.t/ms, ME.v[1], label='v')
+plot(ME.t/ms, ME.v[1]/mV, label='v1')
 subplot(2, 2, 3)
-plot(ME.t/ms, ME.I[0], label='v')
+plot(ME.t/ms, ME.v[2]/mV, label='v2')
 subplot(2, 2, 4)
-plot(MS.t/ms, MS.i, label='v')
-print(MS.t)
-print(MS.i)
+plot(MI.t/ms, MI.v[0]/mV, label='v0')
 show()
 
