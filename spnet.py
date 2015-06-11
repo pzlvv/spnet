b14e47451706dabfca7419110fa5c044ddadd05c
Zane <pz.pzlvv@gmail.com>
2015-06-12 00:34:18 +0800

    Merge example

--- spnet.py
+++ spnet.py
@@ -1,70 +1,77 @@
 from brian2 import *
 prefs.codegen.target = 'numpy'
 
-class BaseNeuronGroup(NeuronGroup):
-    def __init__(self, N, a, b, c, d, isInhibitory=False, threshold_voltage=30*mV):
-        self.a = a;
-        self._model = '''
-        dv/dt = (0.04*(v/mV)**2 + 5*v/mV + 140 - u/mV + I/mV)*mV / ms : volt
-        du/dt = %f*(%f*v-u) / ms: volt
-        dI/dt = -log(2)*I/ms: volt 
-        ''' % (a, b)
+class NeuronNetwork:
+    def __init__(self, N, threshold_voltage=30*mV):
+        self.G = NeuronGroup(N, model='''
+            a : 1
+            b : 1
+            c : 1
+            d : 1
+            k : 1
+            dv/dt = (0.04*(v/mV)**2 + 5*v/mV + 140 - u/mV + I/mV)*mV / ms : volt
+            du/dt = a*(b*v-u) / ms: volt
+            dI/dt = -log(2)*I/ms: volt 
+        ''', threshold='v >= %f * mV' % (threshold_voltage/mV), reset='''
+            v = c*mV
+            u = u + d*mV
+        ''')
+        self.G.I = 0
 
-        self._reset = '''
-        v = %f * mV
-        u = u + %f * mV
-        ''' % (c, d)
+        self.S = Synapses(self.G, self.G, model = '''w:volt''', pre='''I += k_pre*w''')
 
-        t = super().__init__(N, model=self._model, threshold='v >= %f*mV' % (threshold_voltage/mV), reset=self._reset)
-        self.u =  self.I = 0
-        self.v = c*mV
+    def assign_RSneurons(self, indices):
+        for i in indices:
+            self.G.a[i] = 0.02
+            self.G.b[i] = 0.2
+            self.G.c[i] = -65
+            self.G.d[i] = 8
+            self.G.k[i] = 1
 
-class RSNeuronGroup(BaseNeuronGroup):
-    def __init__(self, N):
-        super().__init__(N, 0.02, 0.2, -65, 8)
+    def assign_FSneurons(self, indices):
+        for i in indices:
+            self.G.a[i] = 0.1
+            self.G.b[i] = 0.2
+            self.G.c[i] = -65
+            self.G.d[i] = 2
+            self.G.k[i] = -1
+    
+    def establish_connections(self, *args, **kargs):
+        self.S.connect(*args, **kargs)
 
-class FSNeuronGroup(BaseNeuronGroup):
-    def __init__(self, N):
-        super().__init__(N, 0.1, 0.2, -65, 2)
+
+net = NeuronNetwork(1000)
+net.assign_RSneurons(range(800))
+net.assign_FSneurons(range(800,1000))
+
+net.establish_connections('i!=j', p=0.1)
+net.S.w = 5*mV
+net.S.delay = 'rand() * 20*ms'
 
 
-NE = 3
-NI = 1
+#M = StateMonitor(net.G, ('v','u','I'), record=[0, 1, 2, 3])
+mon = SpikeMonitor(net.G)
 
-P = RSNeuronGroup(3)
-Q = RSNeuronGroup(1)
-G = SpikeGeneratorGroup(1, array([0,0]), array([500, 800])*ms)
 
-SG1 = Synapses(G, P, model = 'w : volt', pre='I += w')
-SG2 = Synapses(G, Q, model = 'w : volt', pre='I += w')
 
-SI = Synapses(Q, P, model = '''w : volt''', pre='''I -= w''')
-SE = Synapses(P, P, model = '''w : volt''', pre='''I += w''')
+@network_operation(dt = 1*ms)
+def update_active(t):
+    net.G.I[randint(0, 1000)] = 20 * mV
+m = Network(collect())
+m.add(net.G)
+m.add(net.S)
+m.add(update_active)
+m.run(1000*ms, report='text')
 
-SG1.connect([0], [0])
-SG2.connect([0], [0])
-SG1.w = 30*mV
-SG2.w = 30*mV
-SE.connect([0, 0], [1, 2])
-SI.connect([0, 0], [1, 2])
-SE.w = array([30, 30])*mV
-SI.w = array([20, 20])*mV
-SE.delay = array([10, 10])*ms
-SI.delay = array([10, 30])*ms
+#subplot(2, 2, 1)
+#plot(M.t/ms, M.v[0]/mV, label='v')
+#subplot(2, 2, 2)
+#plot(M.t/ms, M.v[1]/mV, label='v')
+#subplot(2, 2, 3)
+#plot(M.t/ms, M.v[2]/mV, label='v')
+#subplot(2, 2, 4)
+#plot(M.t/ms, M.v[3]/mV, label='v')
 
-MS = SpikeMonitor(G)
-ME = StateMonitor(P, ('v','u','I'), record=[0, 1, 2])
-MI = StateMonitor(Q, ('v',), record=[0])
-
-run(1000*ms)
-
-subplot(2, 2, 1)
-plot(ME.t/ms, ME.v[0]/mV, label='v0')
-subplot(2, 2, 2)
-plot(ME.t/ms, ME.v[1]/mV, label='v1')
-subplot(2, 2, 3)
-plot(ME.t/ms, ME.v[2]/mV, label='v2')
-subplot(2, 2, 4)
-plot(MI.t/ms, MI.v[0]/mV, label='v0')
+plot(mon.t / ms, mon.i, ',')
 show()
 
